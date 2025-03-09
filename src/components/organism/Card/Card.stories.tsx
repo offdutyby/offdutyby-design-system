2a8264d659b88a9d9e7dce6651b226b79ab4b9f4
offdutyby <offdutybyblo@gmail.com>
2025-03-09 15:17:20 +0900

    feat: Card Component & stories setting

--- src/components/organism/Card/Card.stories.tsx
+++ src/components/organism/Card/Card.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import Card from ".";
+import ListItem from "@/components/atoms/ListItem";
 const meta: Meta<typeof Card> = {
   title: "Components/Card",
   component: Card,
@@ -13,7 +14,32 @@ const meta: Meta<typeof Card> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
-  args: {},
+  args: {
+    header: {
+      title: "혜택받기",
+      description: "17분 전",
+    },
+    slider: {
+      totalSlide: 2,
+      slideWidth: 358,
+      slides: [
+        <ListItem
+          left_title="홈플러스"
+          left_description="10.06 - 10.12"
+          right_title="3000원"
+          right_description="청구결제"
+          iconName="illustration_icon-coin"
+        />,
+        <ListItem
+          left_title="홈플러스"
+          left_description="10.06 - 10.12"
+          right_title="3000원"
+          right_description="청구결제"
+          iconName="illustration_icon-coin"
+        />,
+      ],
+    },
+  },
 };
 
 Primary.decorators = [
--- src/components/organism/Card/index.tsx
+++ src/components/organism/Card/index.tsx
@@ -1,36 +1,31 @@
 import CardHeader from "@/components/atoms/CardHeader";
-import ListItem from "@/components/atoms/ListItem";
-import PaginationDots from "@/components/molecules/PaginationDots";
 import styles from "./index.module.scss";
 import Box from "@/components/molecules/Box";
 import Slider from "@/components/organism/Slider";
-const Card = () => {
+import { ReactNode } from "react";
+
+interface CardProps {
+  header: {
+    title: string;
+    description: string;
+  };
+  slider: {
+    totalSlide: number;
+    slideWidth: number;
+    slides: ReactNode[];
+  };
+}
+
+const Card = ({ header, slider }: CardProps) => {
   return (
     <Box>
       <div className={styles.container}>
-        <CardHeader title="혜택받기" description="17분 전" />
+        <CardHeader title={header.title} description={header.description} />
         <Slider
-          totalSlide={2}
-          slideWidth={358}
-          slides={[
-            <ListItem
-              left_title="홈플러스"
-              left_description="10.06 - 10.12"
-              right_title="3000원"
-              right_description="청구결제"
-              iconName="illustration_icon-coin"
-            />,
-            <ListItem
-              left_title="홈플러스"
-              left_description="10.06 - 10.12"
-              right_title="3000원"
-              right_description="청구결제"
-              iconName="illustration_icon-coin"
-            />,
-          ]}
+          totalSlide={slider.totalSlide}
+          slideWidth={slider.slideWidth}
+          slides={slider.slides}
         />
-
-        <PaginationDots totalPages={2} currentPage={1} />
       </div>
     </Box>
   );
